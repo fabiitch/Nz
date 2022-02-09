e48e3334f4d07a2a3310c722787d1852e8128579
Fabiitch <fabien.occelli@hotmail.fr>
2022-02-09 01:16:23 +0100

    nz logger

---
+++ src/main/java/com/fabiitch/nz/logger/NzLogger.java
@@ -0,0 +1,20 @@
+package com.fabiitch.nz.logger;
+
+public class NzLogger {
+
+    public static void log(int level, String tag, String msg) {
+
+    }
+
+    public static void debug(String tag, String msg) {
+
+    }
+
+    public static void info(String tag, String msg) {
+
+    }
+
+    public static void error(String tag, String msg) {
+
+    }
+}
