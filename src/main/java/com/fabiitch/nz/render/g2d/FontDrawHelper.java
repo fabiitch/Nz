40adc682af73b3419278134595f02ae34b07a48e
Fabiitch <fabien.occelli@hotmail.fr>
2022-04-20 01:21:08 +0200

    fontdrawer

---
+++ src/main/java/com/fabiitch/nz/render/g2d/FontDrawHelper.java
@@ -0,0 +1,38 @@
+package com.fabiitch.nz.render.g2d;
+
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.GlyphLayout;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Vector2;
+
+public class FontDrawHelper {
+    public BitmapFont font;
+    public SpriteBatch sb;
+    GlyphLayout layout = new GlyphLayout();
+
+    public FontDrawHelper(BitmapFont font, SpriteBatch sb) {
+        this.font = font;
+        this.sb = sb;
+    }
+
+    public void drawAt(Vector2 pos, String str) {
+        drawAt(pos.x, pos.y, str);
+    }
+
+    public void drawAt(float x, float y, String str) {
+        font.draw(sb, str, x, y);
+    }
+
+    public void drawCenter(Vector2 pos, String str) {
+        drawCenter(pos.x, pos.y, str);
+    }
+
+    public void drawCenter(float x, float y, String str) {
+        layout.setText(font, str);
+        font.draw(sb, str,
+                x - layout.width / 2,
+                y - layout.height / 2);
+    }
+
+
+}
