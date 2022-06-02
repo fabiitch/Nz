216ef37f4abf90c3a462aa7c2eca2b75a0f4bffc
Fabiitch <fabien.occelli@hotmail.fr>
2022-06-02 23:11:44 +0200

    PosSize dto

---
+++ src/main/java/com/fabiitch/nz/scene2D/nz/NzPlacement.java
@@ -0,0 +1,13 @@
+package com.fabiitch.nz.scene2D.nz;
+
+import com.fabiitch.nz.scene2D.data.PosSize;
+
+public class NzPlacement extends PosSize {
+
+    public boolean sizePercent;
+    public boolean posPercent;
+
+    public boolean witdhAsPercentTotal;
+
+
+}
