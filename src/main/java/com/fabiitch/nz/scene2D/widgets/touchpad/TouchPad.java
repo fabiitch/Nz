117599bfc6f344ddd67bdc50059822d3375aa545
Fabiitch <fabien.occelli@hotmail.fr>
2022-09-19 22:57:41 +0200

    merged

--- src/main/java/com/fabiitch/nz/scene2D/widgets/touchpad/TouchPad.java
+++ src/main/java/com/fabiitch/nz/scene2D/widgets/touchpad/TouchPad.java
@@ -20,6 +20,8 @@ public abstract class TouchPad {
     public Image imageBase;//TODO
     public Image imageKnob;
 
+    public boolean isActive;
+
     protected final Vector2 posBase = new Vector2();
     protected final Vector2 posKnob = new Vector2();
     protected final Vector2 posInactive;
@@ -36,7 +38,7 @@ public abstract class TouchPad {
                     Texture textureBase, Texture textureKnob,
                     Vector2 posInactive,
                     float sizeBase, float sizeKnob,
-                    boolean fixedOnDrag) {
+                    boolean fixedOnTouchDown, boolean fixedOnDrag) {
         this.nzStage = nzStage;
         this.textureBase = textureBase;
         this.textureKnob = textureKnob;
@@ -45,6 +47,7 @@ public abstract class TouchPad {
 
         this.posInactive = posInactive;
         this.fixedOnDrag = fixedOnDrag;
+        this.fixedOnTouchDown = fixedOnTouchDown;
         this.init(textureBase, textureKnob);
     }
 
@@ -59,7 +62,7 @@ public abstract class TouchPad {
         positionner = nzStage.getPositionner(imageKnob, true);
         positionner.setSize(sizeKnob).setPosition(posInactive.x, posInactive.y);
         nzStage.addActor(imageKnob);
-        desactive();
+        touchUp();
     }
 
     public void updateDirForce() {
@@ -68,32 +71,38 @@ public abstract class TouchPad {
         this.force = Percentage.getAlpha(dst, sizeBase / 2);
     }
 
-    public void desactive() {
-        imageKnob.setColor(1, 1, 1, 0.2f);
-        imageBase.setColor(1, 1, 1, 0.2f);
-        nzStage.getPositionner(imageBase, true).setPosition(posInactive.x, posInactive.y);
-        nzStage.getPositionner(imageKnob, true).setPosition(posInactive.x, posInactive.y);
-    }
 
     public void active() {
+        isActive = true;
         imageKnob.setColor(1, 1, 1, 1);
         imageBase.setColor(1, 1, 1, 1);
     }
 
-    public void moveTouchPad(float x, float y) {
-        NzActorPositionner positionner = nzStage.getPositionner(imageBase, true);
-        positionner.setPosition(x, y);
+    public boolean touchDown(float x, float y) {
+        if (fixedOnTouchDown) {
+            float dstToBase = posBase.dst(x, y);
+            if (dstToBase > sizeBase / 2)
+                return false;
+        } else {
+            NzActorPositionner positionner = nzStage.getPositionner(imageBase, true);
+            positionner.setPosition(x, y);
+        }
+        active();
+
         nzStage.getPositionner(imageBase, true).getPosition(posBase);
-        moveKnob(x, y);
+        touchDragged(x, y);
+        return true;
     }
 
-    public void moveKnob(float x, float y) {
+    public void touchDragged(float x, float y) {
+        if(!isActive)
+            return;
         NzActorPositionner positionner = nzStage.getPositionner(imageKnob, true);
         positionner.setPosition(x, y);
         positionner.getPosition(posKnob);
         float dstToBase = posKnob.dst(posBase);
         if (dstToBase > sizeBase / 2) {
-            Vector2 directionTo = V.directionTo(posKnob, posBase, internalV2); //TODO newV2
+            Vector2 directionTo = V.directionTo(posKnob, posBase, internalV2);
             if (fixedOnDrag) {
                 posKnob.set(posBase).add(directionTo.scl(sizeBase / 2));
                 nzStage.getPositionner(imageKnob, true).setPosition(posKnob);
@@ -103,6 +112,18 @@ public abstract class TouchPad {
             }
         }
         this.updateDirForce();
+    }
+
+    public void touchUp() {
+        direction.setZero();
+        force = 0;
+        isActive = false;
+
+        imageKnob.setColor(1, 1, 1, 0.2f);
+        imageBase.setColor(1, 1, 1, 0.2f);
+
+        nzStage.getPositionner(imageBase, true).setPosition(posInactive.x, posInactive.y);
+        nzStage.getPositionner(imageKnob, true).setPosition(posInactive.x, posInactive.y);
     }
 
     public void activeOnStage() {
