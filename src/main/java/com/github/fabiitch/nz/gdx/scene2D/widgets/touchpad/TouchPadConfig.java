bbf2fea2ef957e8322fa5403dcb12d3c3498d767
fabiitch <fabiitch@users.noreply.github.com>
2024-09-18 17:52:50 +0200

    autoPush

---
+++ src/main/java/com/github/fabiitch/nz/gdx/scene2D/widgets/touchpad/TouchPadConfig.java
@@ -0,0 +1,14 @@
+package com.github.fabiitch.nz.gdx.scene2D.widgets.touchpad;
+
+public class TouchPadConfig {
+
+
+    private boolean fixedOnTouchDown;
+    private boolean fixedOnDrag;
+
+    private int maxDstActivation;
+
+
+    private float alphaPressed;
+    private float alphaDisabled;
+}
