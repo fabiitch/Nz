5a29ab1494d3f6f377f5affdfc9fa56d70bc4a7a
fabien occelli <fabien.occelli@hidglobal.com>
2024-09-18 17:52:50 +0200

    autoPush

--- src/main/java/com/github/fabiitch/nz/gdx/scene2D/widgets/touchpad/TouchPad.java
+++ src/main/java/com/github/fabiitch/nz/gdx/scene2D/widgets/touchpad/TouchPad.java
@@ -2,11 +2,12 @@ package com.github.fabiitch.nz.gdx.scene2D.widgets.touchpad;
 
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.math.Vector3;
 import com.badlogic.gdx.scenes.scene2d.ui.Image;
-import com.github.fabiitch.nz.java.math.utils.Percentage;
-import com.github.fabiitch.nz.java.math.vectors.V;
 import com.github.fabiitch.nz.gdx.scene2D.nz.NzActorPositionner;
 import com.github.fabiitch.nz.gdx.scene2D.nz.NzStage;
+import com.github.fabiitch.nz.java.math.utils.Percentage;
+import com.github.fabiitch.nz.java.math.vectors.V;
 
 public abstract class TouchPad {
 
@@ -20,7 +21,8 @@ public abstract class TouchPad {
     public Image imageBase;//TODO
     public Image imageKnob;
 
-    public boolean isActive;
+    public boolean pressed;
+    private boolean disabled;
 
     protected final Vector2 posBase = new Vector2();
     protected final Vector2 posKnob = new Vector2();
@@ -65,17 +67,31 @@ public abstract class TouchPad {
         touchUp();
     }
 
-    public void updateDirForce() {
+    private void updateDirForce() {
         this.direction.set(posKnob).sub(posBase).nor();
         float dst = posBase.dst(posKnob);
         this.force = Percentage.alpha(dst, sizeBase / 2);
     }
 
 
-    public void active() {
-        isActive = true;
+    public void pressed() {
+        pressed = true;
         imageKnob.setColor(1, 1, 1, 1);
         imageBase.setColor(1, 1, 1, 1);
+    }
+
+    public void unPressed() {
+        pressed = false;
+
+        imageKnob.setColor(1, 1, 1, 0.2f);
+        imageBase.setColor(1, 1, 1, 0.2f);
+    }
+
+    public void resetPosition() {
+        direction.setZero();
+        force = 0;
+        nzStage.getPositionner(imageBase, true).setPosition(posInactive.x, posInactive.y);
+        nzStage.getPositionner(imageKnob, true).setPosition(posInactive.x, posInactive.y);
     }
 
     public boolean touchDown(float x, float y) {
@@ -87,7 +103,7 @@ public abstract class TouchPad {
             NzActorPositionner positionner = nzStage.getPositionner(imageBase, true);
             positionner.setPosition(x, y);
         }
-        active();
+        pressed();
 
         nzStage.getPositionner(imageBase, true).getPosition(posBase);
         touchDragged(x, y);
@@ -95,7 +111,7 @@ public abstract class TouchPad {
     }
 
     public void touchDragged(float x, float y) {
-        if(!isActive)
+        if (!pressed)
             return;
         NzActorPositionner positionner = nzStage.getPositionner(imageKnob, true);
         positionner.setPosition(x, y);
@@ -114,24 +130,39 @@ public abstract class TouchPad {
         this.updateDirForce();
     }
 
-    public void touchUp() {
-        direction.setZero();
-        force = 0;
-        isActive = false;
-
-        imageKnob.setColor(1, 1, 1, 0.2f);
-        imageBase.setColor(1, 1, 1, 0.2f);
-
-        nzStage.getPositionner(imageBase, true).setPosition(posInactive.x, posInactive.y);
-        nzStage.getPositionner(imageKnob, true).setPosition(posInactive.x, posInactive.y);
+    public Vector3 compute(float x, float y, Vector3 result) {
+        Vector2 knobPos = new Vector2(x, y);
+        float dstToBase = knobPos.dst(posBase);
+        Vector2 direction = this.internalV2.set(knobPos).sub(posBase).nor();
+        float force = Percentage.alpha(dstToBase, sizeBase / 2);
+        force = Math.max(1, force);
+        result.set(direction, force);
+        return result;
     }
 
-    public void activeOnStage() {
+    public void touchUp() {
+        resetPosition();
+        unPressed();
+    }
+
+
+    public void setStickPos(Vector2 direction, float force) {
+        Vector2 center = posBase.cpy();
+        float dstToAdd = force * sizeBase / 2;
+        Vector2 pos = center.cpy().add(new Vector2(1, 0).setAngleDeg(direction.angleDeg()).setLength(dstToAdd));
+        if (this.pressed) {
+            touchDragged(pos.x, pos.y);
+        } else {
+            touchDown(pos.x, pos.y);
+        }
+    }
+
+    public void addToStage() {
         nzStage.addActor(imageBase);
         nzStage.addActor(imageKnob);
     }
 
-    public void desactiveOnStage() {
+    public void removeToStage() {
         imageBase.remove();
         imageKnob.remove();
     }
