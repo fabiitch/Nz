92ac9ca32b2d4be04434ee55d4c6acf1a70a9cce
fabiitch <fabien.occelli@hotmail.fr>
2024-10-22 01:25:59 +0200

    autoPush

--- src/test/java/com/github/fabiitch/nz/demo/shapes/path/RectangleSmoothPathDemo.java
+++ src/test/java/com/github/fabiitch/nz/demo/shapes/path/RectangleSmoothPathDemo.java
@@ -1,4 +1,4 @@
-package com.github.fabiitch.nz.demo.shapes;
+package com.github.fabiitch.nz.demo.shapes.path;
 
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
@@ -11,38 +11,25 @@ import com.github.fabiitch.nz.gdx.debug.DebugDisplayUtils;
 import com.github.fabiitch.nz.gdx.debug.huddebug.HudDebug;
 import com.github.fabiitch.nz.gdx.log.StrFormat;
 import com.github.fabiitch.nz.java.data.Pair;
-import com.github.fabiitch.nz.java.math.shapes.utils.rectangle.path.RectanglePath;
 import com.github.fabiitch.nz.java.math.shapes.utils.rectangle.path.RectanglePathSmooth;
 import com.github.fabiitch.nz.java.math.shapes.utils.rectangle.path.RectanglePathStep;
-import com.github.fabiitch.nz.java.math.utils.direction.Direction;
 import com.github.fabiitch.nz.java.utils.Randoms;
 
 @DemoScreen(group = "math.shape2D.rectangle.path")
-public class RectanglePathDemo extends BaseTryScreen {
+public class RectangleSmoothPathDemo extends BaseTryScreen {
 
     Array<Pair<Color, Rectangle>> colorRectArray = new Array<>();
     Vector2 start = new Vector2(100, 100);
 
-    public RectanglePathDemo() {
+    public RectangleSmoothPathDemo() {
         super();
-        RectanglePath rectanglePath = new RectanglePathSmooth();
-        rectanglePath.add(Direction.Right, 200, 50);
-        rectanglePath.add(Direction.Top, 100, 20);
-
-        rectanglePath.add(Direction.Top, 100, 40);
-        rectanglePath.add(Direction.Left, 100, 20);
-        rectanglePath.add(Direction.Bot, 100, 20);
-        rectanglePath.add(Direction.Left, 100, 10);
-        rectanglePath.add(Direction.Top, 200, 50);
-        rectanglePath.add(Direction.Right, 300, 10);
-        rectanglePath.add(Direction.Bot, 50, 5);
-        rectanglePath.add(Direction.Bot, 50, 25);
-        rectanglePath.add(Direction.Right, 70, 5);
-        rectanglePath.add(Direction.Bot, 150, 25);
+        Array<RectanglePathStep> rectanglePathSteps = DemoPathUtils.pathStep();
+        RectanglePathSmooth smoothPath = new RectanglePathSmooth();
+        smoothPath.getArray().addAll(rectanglePathSteps);
 
         int i = 0;
-        Array<Rectangle> compute = rectanglePath.compute(start);
-        for (RectanglePathStep step : rectanglePath.getArray()) {
+        Array<Rectangle> compute = smoothPath.compute(start);
+        for (RectanglePathStep step : smoothPath.getArray()) {
             Color color = Randoms.color();
             String format = StrFormat.format("Dir={}, length={}, size={}", step.getDirection(), step.getLength(), step.getSize());
             HudDebug.addTopRight(i + " ", format, color);
