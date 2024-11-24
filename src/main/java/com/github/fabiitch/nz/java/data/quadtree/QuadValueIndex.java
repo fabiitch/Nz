ae838599af1242cc52ea64e0eb32c82077cfd921
fabiitch <fabiitch@users.noreply.github.com>
2024-11-24 17:02:47 +0100

    autoPush

---
+++ src/main/java/com/github/fabiitch/nz/java/data/quadtree/QuadValueIndex.java
@@ -0,0 +1,10 @@
+package com.github.fabiitch.nz.java.data.quadtree;
+
+import com.badlogic.gdx.math.Rectangle;
+
+public interface QuadValueIndex {
+
+    int getId();
+
+    Rectangle getBounds();
+}
