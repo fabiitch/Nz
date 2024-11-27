ca9d46f3c772fb9074cb8144e7208c9dad3fa5ec
fabiitch <fabien.occelli@hotmail.fr>
2024-11-27 14:53:56 +0100

    test commit author

--- src/main/java/com/github/fabiitch/nz/java/exception/NzBadValueException.java
+++ src/main/java/com/github/fabiitch/nz/java/exception/NzBadValueException.java
@@ -1,8 +1,8 @@
 package com.github.fabiitch.nz.java.exception;
 
-public class NzBadValueException extends NzRuntimeException{
+public class NzBadValueException extends NzRuntimeException {
 
-    public NzBadValueException(String message ,Object... params) {
-        super(message);
+    public NzBadValueException(String message, Object... params) {
+        super(message, params);
     }
 }
