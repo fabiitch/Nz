83cb4e9bf0fa2703b8da34701ed1336ec2f772e9
fabiitch <fabiitch@users.noreply.github.com>
2024-12-03 18:07:09 +0100

    autoPush

--- src/main/java/com/github/fabiitch/nz/java/time/timers/CountDown.java
+++ src/main/java/com/github/fabiitch/nz/java/time/timers/CountDown.java
@@ -1,10 +1,12 @@
 package com.github.fabiitch.nz.java.time.timers;
 
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
+@NoArgsConstructor
 public class CountDown {
     private float duration;
     private float acc = 0;
