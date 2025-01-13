931e8f0ee8fe387b30505d695d74c3aa4a328d30
fabiitch <fabiitch@users.noreply.github.com>
2025-01-13 16:57:11 +0100

    autoPush

--- src/main/java/com/github/fabiitch/nz/gdx/render/fonts/NzFontRender.java
+++ src/main/java/com/github/fabiitch/nz/gdx/render/fonts/NzFontRender.java
@@ -5,24 +5,33 @@ import com.badlogic.gdx.graphics.g2d.GlyphLayout;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.math.Vector2;
 
-//TODO remove
-public class FontUtils {
+public class NzFontRender {
+    private final GlyphLayout glyphLayout = new GlyphLayout();
+    private final BitmapFont font;
+    private final SpriteBatch sb;
 
-    private static final GlyphLayout glyphLayout = new GlyphLayout();
+    public NzFontRender(SpriteBatch sb, BitmapFont font) {
+        this.sb = sb;
+        this.font = font;
+    }
 
-    public static Vector2 toDrawCenter(BitmapFont font, Vector2 pos, String str, Vector2 result) {
+    public Vector2 toDrawCenter(Vector2 pos, String str, Vector2 result) {
         glyphLayout.setText(font, str);
         return result.set(pos.x - glyphLayout.width / 2, pos.y + glyphLayout.height / 2);
     }
 
-    public static Vector2 toDrawCenter(BitmapFont font, float x, float y, String str, Vector2 result) {
+    public Vector2 toDrawCenter(float x, float y, String str, Vector2 result) {
         glyphLayout.setText(font, str);
         return result.set(x - glyphLayout.width / 2, y + glyphLayout.height / 2);
     }
 
-    public static void drawCenter(float x, float y, String str, BitmapFont font, SpriteBatch sb) {
+    public void drawCenter(float x, float y, String str) {
         glyphLayout.setText(font, str);
         font.draw(sb, str,
                 x - glyphLayout.width / 2, y + glyphLayout.height / 2);
+    }
+
+    public void drawCenter(Vector2 position, String str) {
+        drawCenter(position.x, position.y, str);
     }
 }
