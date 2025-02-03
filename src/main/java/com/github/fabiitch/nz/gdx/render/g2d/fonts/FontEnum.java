73ab3a531b56f7990226bc38ef0da0ae4e5d84d1
fabiitch <fabiitch@users.noreply.github.com>
2025-02-03 17:05:11 +0100

    autoPush

--- src/main/java/com/github/fabiitch/nz/gdx/render/g2d/fonts/FontEnum.java
+++ src/main/java/com/github/fabiitch/nz/gdx/render/g2d/fonts/FontEnum.java
@@ -1,5 +1,16 @@
 package com.github.fabiitch.nz.gdx.render.g2d.fonts;
 
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
 public enum FontEnum {
-    CD, Info
+    CD("OpenSans-Bold"),
+    Info("OpenSans-Bold");
+
+    private final String fileName;
+
+    public String getFileName() {
+        return "fonts/" + fileName + ".ttf";
+    }
+
 }
