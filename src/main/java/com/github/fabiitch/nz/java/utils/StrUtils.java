0edccc8c5e5064c03f56fb3c230b418434726f7a
fabiitch <fabien.occelli@hotmail.fr>
2025-02-18 17:54:12 +0100

    autoPush

--- src/main/java/com/github/fabiitch/nz/java/utils/StrUtils.java
+++ src/main/java/com/github/fabiitch/nz/java/utils/StrUtils.java
@@ -4,4 +4,8 @@ public class StrUtils {
     public static String replaceAllNonAlphanumeric(String str) {
         return str.replaceAll("[^A-Za-z0-9]", "");
     }
+
+    public static String removeBegin(String str, String begin) {
+        return str.substring(0, begin.length() - 1);
+    }
 }
