e56d3b95390520770f57335dee38f024ed920393
fabiitch <fabiitch@users.noreply.github.com>
2025-02-19 16:58:37 +0100

    autoPush

--- src/main/java/com/github/fabiitch/nz/java/utils/StrUtils.java
+++ src/main/java/com/github/fabiitch/nz/java/utils/StrUtils.java
@@ -8,4 +8,12 @@ public class StrUtils {
     public static String removeBegin(String str, String begin) {
         return str.substring(0, begin.length() - 1);
     }
+
+    public static boolean isEmpty(String str) {
+        return str == null || str.isEmpty();
+    }
+
+    public static boolean isNotEmpty(String str) {
+        return !isEmpty(str);
+    }
 }
