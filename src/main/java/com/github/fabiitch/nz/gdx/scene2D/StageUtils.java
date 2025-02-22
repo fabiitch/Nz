f8686cbc5211f80ddbeb097e60ce9080497a9de3
fabiitch <fabiitch@users.noreply.github.com>
2025-02-23 00:43:18 +0100

    autoPush

--- src/main/java/com/github/fabiitch/nz/gdx/scene2D/StageUtils.java
+++ src/main/java/com/github/fabiitch/nz/gdx/scene2D/StageUtils.java
@@ -1,15 +1,21 @@
 package com.github.fabiitch.nz.gdx.scene2D;
 
 import com.badlogic.gdx.math.Rectangle;
+import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.scenes.scene2d.Actor;
+import com.badlogic.gdx.scenes.scene2d.Group;
 import com.badlogic.gdx.scenes.scene2d.Touchable;
 import com.badlogic.gdx.scenes.scene2d.ui.Button;
 import com.badlogic.gdx.scenes.scene2d.ui.WidgetGroup;
 import com.badlogic.gdx.utils.SnapshotArray;
+import com.github.fabiitch.nz.gdx.scene2D.nz.StagePositionnerUtils;
 import com.github.fabiitch.nz.java.math.shapes.utils.RectangleUtils;
 
 
 public class StageUtils {
+
+    private static Vector2 tmp = new Vector2();
+
     public static void buttonFullDisabled(Button button, boolean b) {
         button.setTouchable(b ? Touchable.disabled : Touchable.enabled);
         button.setDisabled(b);
@@ -30,18 +36,33 @@ public class StageUtils {
         return result.set(actor.getX(), actor.getY(), actor.getWidth(), actor.getHeight());
     }
 
+
+    public static Vector2 getCenter(Actor actor) {
+        return tmp.set(actor.getX() + actor.getWidth() / 2, actor.getY() + actor.getHeight() / 2);
+    }
+
+    public static void centerOnParent(Actor actor) {
+        Group parent = actor.getParent();
+        if (parent != null) {
+//            StagePositionnerUtils.center(actor);
+//            Vector2 center = getCenter(parent);
+//            actor.setPosition(center.x, center.y);
+//            actor.setPosition(center.x + actor.getWidth() / 2, center.y + actor.getHeight() / 2);
+        }
+    }
+
     public static void fitSizeOnChildren(WidgetGroup widgetGroup) {
         SnapshotArray<Actor> children = widgetGroup.getChildren();
         Rectangle rectangle = null;
 
-        if(children.size > 0) {
+        if (children.size > 0) {
             for (Actor child : children) {
                 if (rectangle == null)
                     rectangle = getBounds(child);
                 else
-                    RectangleUtils.mergeFloorCeil(rectangle, getBounds(child));
+                    rectangle.merge(getBounds(child));
             }
-            widgetGroup.setPosition(rectangle.x, rectangle.y);
+//            widgetGroup.setPosition(rectangle.x, rectangle.y);
             widgetGroup.setSize(rectangle.width, rectangle.height);
         }
     }
