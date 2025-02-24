f98175b7a582065fd73019dfc4b500e199b21091
fabiitch <fabien.occelli@hotmail.fr>
2025-02-24 18:12:50 +0100

    autoPush

---
+++ src/main/java/com/github/fabiitch/nz/gdx/scene2D/nz/NzPlacementType.java
@@ -0,0 +1,5 @@
+package com.github.fabiitch.nz.gdx.scene2D.nz;
+
+public enum NzPlacementType {
+    Fix, Percent
+}
--- src/main/java/com/github/fabiitch/nz/java/data/collections/utils/ListUtils.java
+++ src/main/java/com/github/fabiitch/nz/java/data/collections/utils/ListUtils.java
@@ -1,13 +1,24 @@
 package com.github.fabiitch.nz.java.data.collections.utils;
 
+import lombok.experimental.UtilityClass;
+
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+@UtilityClass
 public class ListUtils {
     public static <T> List<T> asList(T... values) {
         Arrays.asList(values);
         List<T> list = new ArrayList<>(Arrays.asList(values));
         return list;
+    }
+
+    public static boolean isEmpty(List<?> list) {
+        return list != null && list.isEmpty();
+    }
+
+    public static boolean isNotEmpty(List<?> list) {
+        return !isEmpty(list);
     }
 }
