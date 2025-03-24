8fe8ab264eaae0aa98a6719ab8d5a1d590494416
fabiitch <fabiitch@users.noreply.github.com>
2025-03-24 16:14:02 +0100

    autoPush

--- src/main/java/com/github/fabiitch/nz/gdx/render/ColorUtils.java
+++ src/main/java/com/github/fabiitch/nz/gdx/render/ColorUtils.java
@@ -11,8 +11,13 @@ public class ColorUtils {
     public static Color set(Color source, Color target, float alpha) {
         return source.set(target.r, target.g, target.b, alpha);
     }
+
     public static Color setAlpha(Color color, float alpha) {
         color.a = alpha;
         return color;
+    }
+
+    public static Color setAlphaCpy(Color color, float alpha) {
+        return setAlpha(color.cpy(), alpha);
     }
 }
