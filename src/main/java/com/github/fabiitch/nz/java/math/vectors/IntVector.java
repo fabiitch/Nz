c90d0bec47c00bc43554bfb1067ee1f80cd1c3e9
fabiitch <fabien.occelli@hotmail.fr>
2025-04-29 18:41:03 +0200

    autoPush

--- src/main/java/com/github/fabiitch/nz/java/math/vectors/IntVector.java
+++ src/main/java/com/github/fabiitch/nz/java/math/vectors/IntVector.java
@@ -3,15 +3,22 @@ package com.github.fabiitch.nz.java.math.vectors;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
 
-@Getter
-@Setter
 @NoArgsConstructor
 @AllArgsConstructor
 public class IntVector {
 
-    private int x, y;
+    public int x, y;
+
+    public IntVector setX(int x) {
+        this.x = x;
+        return this;
+    }
+
+    public IntVector setY(int y) {
+        this.y = y;
+        return this;
+    }
 
     public IntVector set(int x, int y) {
         this.x = x;
@@ -53,5 +60,34 @@ public class IntVector {
     public IntVector subY(int y) {
         this.y -= y;
         return this;
+    }
+
+    @Override
+    public String toString() {
+        return "(" + this.x + "," + this.y + ")";
+    }
+
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        } else if (obj == null) {
+            return false;
+        } else if (this.getClass() != obj.getClass()) {
+            return false;
+        } else {
+            IntVector other = (IntVector) obj;
+            return this.x == other.x && this.y == other.y;
+        }
+    }
+
+    public boolean equals(IntVector other) {
+        return this.x == other.x && this.y == other.y;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = Integer.hashCode(x);
+        result = 31 * result + Integer.hashCode(y);
+        return result;
     }
 }
