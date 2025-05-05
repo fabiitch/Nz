9658996aa6b0634d33d7b832ecfcb22db15f5822
fabiitch <fabiitch@users.noreply.github.com>
2025-05-05 16:44:44 +0200

    autoPush

---
+++ src/main/java/com/github/fabiitch/nz/gdx/utils/GdxFileUtils.java
@@ -0,0 +1,13 @@
+package com.github.fabiitch.nz.gdx.utils;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.files.FileHandle;
+import lombok.experimental.UtilityClass;
+
+import java.io.File;
+
+@UtilityClass
+public class GdxFileUtils {
+    
+    
+}
