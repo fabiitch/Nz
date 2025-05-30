83fe6a9081e14e1b6ce236d8c4379337bd66ea5e
fabiitch <fabiitch@users.noreply.github.com>
2025-05-30 18:09:10 +0200

    autoPush

--- src/main/java/com/github/fabiitch/nz/gdx/utils/GdxFileUtils.java
+++ src/main/java/com/github/fabiitch/nz/gdx/utils/GdxFileUtils.java
@@ -1,13 +1,24 @@
 package com.github.fabiitch.nz.gdx.utils;
 
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.files.FileHandle;
 import lombok.experimental.UtilityClass;
-
-import java.io.File;
 
 @UtilityClass
 public class GdxFileUtils {
-    
-    
+
+    public static boolean internalExist(final String path) {
+        return Gdx.files.internal(path).exists();
+    }
+
+    public static boolean absoluteExist(final String path) {
+        return Gdx.files.absolute(path).exists();
+    }
+
+    public static boolean external(final String path) {
+        return Gdx.files.external(path).exists();
+    }
+
+    public static boolean local(final String path) {
+        return Gdx.files.local(path).exists();
+    }
 }
