cff594dc4b5b0eb4e60d91b5c6eed3b43457c54b
fabiitch <fabiitch@users.noreply.github.com>
2025-06-23 17:15:11 +0200

    autoPush

--- src/main/java/com/github/fabiitch/nz/gdx/camera/CameraUtils.java
+++ src/main/java/com/github/fabiitch/nz/gdx/camera/CameraUtils.java
@@ -1,11 +1,29 @@
 package com.github.fabiitch.nz.gdx.camera;
 
+import com.badlogic.gdx.graphics.Camera;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.math.Rectangle;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.math.Vector3;
 import com.github.fabiitch.nz.java.math.shapes.builders.RectangleBuilder;
 import com.github.fabiitch.nz.java.math.shapes.utils.RectangleUtils;
+import com.github.fabiitch.nz.java.math.vectors.V3;
+import com.github.fabiitch.nz.java.math.vectors.v2.V2;
 
 public class CameraUtils {
+
+    public static  Rectangle unProject(Camera camera, Rectangle rectangle) {
+        Vector3 tmp = new Vector3();
+        Vector3 tmp = new Vector3();
+
+     RectangleBuilder.
+    }
+
+    public static Vector2 unproject(Camera camera, Vector2 point) {
+        Vector3 tmp = V3.v(point);
+        Vector3 unproject = camera.unproject(tmp);
+        return V2.v(unproject);
+    }
 
     public static Rectangle toRectangle(OrthographicCamera camera, Rectangle res) {
         return RectangleUtils.setCenter(res, camera.position.x, camera.position.y,
