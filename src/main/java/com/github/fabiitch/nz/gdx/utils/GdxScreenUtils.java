a91bd35c7fa65eb53769e7d66c07f89e995bf7fd
fabiitch <fabien.occelli@hotmail.fr>
2025-08-18 17:28:30 +0200

    autoPush

--- src/main/java/com/github/fabiitch/nz/gdx/utils/GdxScreenUtils.java
+++ src/main/java/com/github/fabiitch/nz/gdx/utils/GdxScreenUtils.java
@@ -2,12 +2,13 @@ package com.github.fabiitch.nz.gdx.utils;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.math.Vector2;
+import com.github.fabiitch.nz.java.math.percent.Percentage;
 import lombok.experimental.UtilityClass;
 
 @UtilityClass
 public class GdxScreenUtils {
 
-    public static Vector2 getScreenResolution(){
+    public static Vector2 getScreenResolution() {
         return new Vector2(Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
     }
 
@@ -21,5 +22,15 @@ public class GdxScreenUtils {
 
     public static float getScreenCenterY() {
         return Gdx.graphics.getHeight() / 2;
+    }
+
+    public static float getScreenRatio() {
+        return Gdx.graphics.getWidth() / (float) Gdx.graphics.getHeight();
+    }
+
+    public static Vector2 getPercentPos(float percentX, float percentY) {
+        float x = Percentage.value(percentX, Gdx.graphics.getWidth());
+        float y = Percentage.value(percentY, Gdx.graphics.getHeight());
+        return new Vector2(x, y);
     }
 }
