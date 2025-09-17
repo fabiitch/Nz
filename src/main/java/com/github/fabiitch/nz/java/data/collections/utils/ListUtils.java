5aa17f3815d6b81b40b271f0179b8845b95c4042
fabiitch <fabien.occelli@hotmail.fr>
2025-09-18 01:04:36 +0200

    autoPush

--- src/main/java/com/github/fabiitch/nz/java/data/collections/utils/ListUtils.java
+++ src/main/java/com/github/fabiitch/nz/java/data/collections/utils/ListUtils.java
@@ -10,12 +10,15 @@ import java.util.List;
 public class ListUtils {
 
     public static <T> T getFirst(List<T> list) {
-        if(list.isEmpty())
+        if (list.isEmpty())
             return null;
         return list.get(0);
     }
+
     public static <T> T getLast(List<T> list) {
-        return list.get(list.size()-1);
+        if (list.isEmpty())
+            return null;
+        return list.get(list.size() - 1);
     }
 
     public static <T> List<T> takeXLast(List<T> list, int xLast) {
