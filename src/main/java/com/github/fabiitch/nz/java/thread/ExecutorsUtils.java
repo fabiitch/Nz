d8061e0c04e895069f5bacac49b227cffa445c29
fabiitch <fabiitch@users.noreply.github.com>
2025-09-21 20:46:14 +0200

    autoPush

--- src/main/java/com/github/fabiitch/nz/java/thread/ExecutorsUtils.java
+++ src/main/java/com/github/fabiitch/nz/java/thread/ExecutorsUtils.java
@@ -10,22 +10,22 @@ import java.util.concurrent.ScheduledExecutorService;
 public class ExecutorsUtils {
 
     public static ScheduledExecutorService newScheduledThreadPool(String name, int nThreads) {
-        return Executors.newScheduledThreadPool(nThreads, new NamedIncThreadFactory(name));
+        return Executors.newScheduledThreadPool(nThreads, new NamedIncThreadFactory(name, true));
     }
 
     public static ScheduledExecutorService newSingleThreadScheduledExecutor(String name) {
-        return Executors.newSingleThreadScheduledExecutor(new NamedThreadFactory(name));
+        return Executors.newSingleThreadScheduledExecutor(new NamedThreadFactory(name, true));
     }
 
     public static ExecutorService newFixedThreadPool(int nThreads, String name) {
-        return Executors.newFixedThreadPool(nThreads, new NamedThreadFactory(name));
+        return Executors.newFixedThreadPool(nThreads, new NamedThreadFactory(name, true));
     }
 
     public static ExecutorService newSingleThreadExecutor(String name) {
-        return Executors.newSingleThreadExecutor(new NamedThreadFactory(name));
+        return Executors.newSingleThreadExecutor(new NamedThreadFactory(name, true));
     }
 
     public static ExecutorService newCachedThreadPool(String name) {
-        return Executors.newCachedThreadPool(new NamedThreadFactory(name));
+        return Executors.newCachedThreadPool(new NamedThreadFactory(name, true));
     }
 }
