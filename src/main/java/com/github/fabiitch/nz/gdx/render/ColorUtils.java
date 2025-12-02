f645d4659630295412868e2bdf62640f88d224b6
fabiitch <fabiitch@users.noreply.github.com>
2025-12-02 01:25:40 +0100

    autoPush

--- src/main/java/com/github/fabiitch/nz/gdx/render/ColorUtils.java
+++ src/main/java/com/github/fabiitch/nz/gdx/render/ColorUtils.java
@@ -20,4 +20,25 @@ public class ColorUtils {
     public static Color setAlphaCpy(Color color, float alpha) {
         return setAlpha(color.cpy(), alpha);
     }
+
+    public static Color getGradientColor(
+            double value,
+            double min,
+            double max,
+            Color startColor,
+            Color endColor) {
+
+        if (max == min) return startColor;
+
+        // Clamp value
+        double ratio = (value - min) / (max - min);
+        ratio = Math.max(0.0, Math.min(1.0, ratio));
+
+        int r = (int) (startColor.r   + ratio * (endColor.r   - startColor.r));
+        int g = (int) (startColor.g + ratio * (endColor.g - startColor.g));
+        int b = (int) (startColor.b  + ratio * (endColor.b  - startColor.b));
+
+        return new Color(r, g, b, 1);
+    }
+
 }
