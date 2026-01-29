c35089094fa0aeae5e36c269abeaa2288bddda71
fabiitch <fabiitch@users.noreply.github.com>
2026-01-29 16:57:19 +0100

    autoPush

---
+++ src/main/java/com/github/fabiitch/nz/java/event/MultiplexerTS.java
@@ -0,0 +1,17 @@
+package com.github.fabiitch.nz.java.event;
+
+import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
+
+public class MultiplexerTS<T> {
+
+    protected final List<T> listeners = new CopyOnWriteArrayList<>();
+
+    public void addListener(T t) {
+        listeners.add(t);
+    }
+
+    public void removeListener(T t) {
+        listeners.remove(t);
+    }
+}
