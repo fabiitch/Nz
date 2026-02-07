5c4545eab6b313bfaae1f5d3ab872c9448f3a7b7
fabiitch <fabien.occelli@hotmail.fr>
2026-02-08 00:46:29 +0100

    autoPush

--- src/main/java/com/github/fabiitch/nz/gdx/utils/GdxUtils.java
+++ src/main/java/com/github/fabiitch/nz/gdx/utils/GdxUtils.java
@@ -2,6 +2,9 @@ package com.github.fabiitch.nz.gdx.utils;
 
 import com.badlogic.gdx.Application;
 import com.badlogic.gdx.Gdx;
+import com.sun.management.OperatingSystemMXBean;
+
+import java.lang.management.ManagementFactory;
 
 public class GdxUtils {
 
@@ -21,7 +24,7 @@ public class GdxUtils {
         return !isMobile();
     }
 
-    public static String getHeapLog() {
+    public static String getJavaHeapLog() {
         Runtime rt = Runtime.getRuntime();
         long total = rt.totalMemory();
         long free = rt.freeMemory();
@@ -32,4 +35,34 @@ public class GdxUtils {
                 + " / total=" + total / 1024 / 1024 + "MB"
                 + " / max=" + max / 1024 / 1024 + "MB";
     }
+
+
+    public static String getMemoryLog() {
+        Runtime rt = Runtime.getRuntime();
+
+        long heapTotal = rt.totalMemory();
+        long heapFree  = rt.freeMemory();
+        long heapUsed  = heapTotal - heapFree;
+        long heapMax   = rt.maxMemory();
+
+        double nativeUsed = -1;
+
+        try {
+            OperatingSystemMXBean os =
+                    (OperatingSystemMXBean) ManagementFactory.getOperatingSystemMXBean();
+            nativeUsed = os.getProcessCpuLoad(); // dummy init guard
+            nativeUsed = os.getCommittedVirtualMemorySize();
+        } catch (Throwable ignored) {
+            // JVM sans accès aux extensions com.sun.*
+        }
+
+        return String.format(
+                "MEM | heap: used=%dMB total=%dMB max=%dMB | native: committed=%s",
+                heapUsed / 1024 / 1024,
+                heapTotal / 1024 / 1024,
+                heapMax / 1024 / 1024,
+                nativeUsed > 0 ? (nativeUsed / 1024 / 1024 + "MB") : "n/a"
+        );
+    }
+
 }
