2ea4a407adebc7e70f45f93023991d820bba3197
fabiitch <fabiitch@users.noreply.github.com>
2026-02-28 21:48:21 +0100

    autoPush

--- src/main/java/com/github/fabiitch/nz/gdx/utils/GdxUtils.java
+++ src/main/java/com/github/fabiitch/nz/gdx/utils/GdxUtils.java
@@ -35,34 +35,4 @@ public class GdxUtils {
                 + " / total=" + total / 1024 / 1024 + "MB"
                 + " / max=" + max / 1024 / 1024 + "MB";
     }
-
-
-    public static String getMemoryLog() {
-        Runtime rt = Runtime.getRuntime();
-
-        long heapTotal = rt.totalMemory();
-        long heapFree  = rt.freeMemory();
-        long heapUsed  = heapTotal - heapFree;
-        long heapMax   = rt.maxMemory();
-
-        double nativeUsed = -1;
-
-        try {
-            OperatingSystemMXBean os =
-                    (OperatingSystemMXBean) ManagementFactory.getOperatingSystemMXBean();
-            nativeUsed = os.getProcessCpuLoad(); // dummy init guard
-            nativeUsed = os.getCommittedVirtualMemorySize();
-        } catch (Throwable ignored) {
-            // JVM sans accès aux extensions com.sun.*
-        }
-
-        return String.format(
-                "MEM | heap: used=%dMB total=%dMB max=%dMB | native: committed=%s",
-                heapUsed / 1024 / 1024,
-                heapTotal / 1024 / 1024,
-                heapMax / 1024 / 1024,
-                nativeUsed > 0 ? (nativeUsed / 1024 / 1024 + "MB") : "n/a"
-        );
-    }
-
 }
