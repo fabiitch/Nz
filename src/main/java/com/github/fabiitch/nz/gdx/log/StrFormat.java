fdc4f8d0fd9f0ede445e6ff69d14196b1f3d8b90
fabiitch <fabiitch@users.noreply.github.com>
2026-05-26 17:36:25 +0200

    autoPush

--- src/main/java/com/github/fabiitch/nz/gdx/log/StrFormat.java
+++ src/main/java/com/github/fabiitch/nz/gdx/log/StrFormat.java
@@ -4,35 +4,16 @@ import com.badlogic.gdx.utils.Array;
 
 import java.util.StringJoiner;
 import java.util.function.Function;
+import java.util.regex.Matcher;
 
-//TODO LOG4J org/slf4j/helpers/MessageFormatter.java
-//TODO version sb private pour les games
 public class StrFormat {
-    static final String DELIM_STR = "{}";
 
-    private final static StringBuilder SB = new StringBuilder(0);
-
-    //TODO coupe les messages voir com/nzt/mfl/engine/services/sort/OrderGameService.java:18
-    public static String format(String message, Object... args) {
-        SB.setLength(0);
-        int msgIndex = 0;
-        int delimIndex;
-
-        for (int argsCount = 0; argsCount < args.length; argsCount++) {
-            delimIndex = message.indexOf(DELIM_STR, msgIndex);
-
-            if (delimIndex == -1) {//end
-                if (msgIndex == 0)
-                    return message;
-                else
-                    SB.append(message, msgIndex, message.length());
-            } else {
-                SB.append(message, msgIndex, delimIndex);
-                SB.append(args[argsCount]);
-                msgIndex = delimIndex + 2;
-            }
+    public static String format(String msg, Object... params) {
+        for (Object param : params) {
+            String safeParam = Matcher.quoteReplacement(param != null ? param.toString() : "null");
+            msg = msg.replaceFirst("\\{}", safeParam);
         }
-        return SB.toString();
+        return msg;
     }
 
 
